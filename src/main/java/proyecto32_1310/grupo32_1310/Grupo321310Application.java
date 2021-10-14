89a429530f7f22d555bfe51b3fe6abc2f3f93caf
JOSE LUIS ROMERO BACCA <jose.romero1.mt@correo.usa.edu.co>
2021-10-13 21:38:54 -0500

    subir archivos Spring boot

---
+++ src/main/java/proyecto32_1310/grupo32_1310/Grupo321310Application.java
@@ -0,0 +1,13 @@
+package proyecto32_1310.grupo32_1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto32_1310/grupo32_1310/Grupo321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto32_1310.grupo32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
